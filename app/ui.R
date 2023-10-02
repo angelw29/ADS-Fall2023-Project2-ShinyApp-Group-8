467376c1b26d2dce3b8ffd26fd87b7c5de166bd8
angelw29 <yw3800@barnard.edu>
2023-10-01 23:07:05 -0400

    AW commit

--- app/ui.R
+++ app/ui.R
@@ -1,15 +1,15 @@
-
+```{r setup, include=FALSE}
 if (!require("shiny")) {
   install.packages("shiny")
   library(shiny)
 }
-if (!require("shinyWidgets")) {
-  install.packages("shinyWidgets")
-  library(shinyWidgets)
+if (!require("dplyr")) {
+  install.packages("dplyr")
+  library(dplyr)
 }
-if (!require("shinythemes")) {
-  install.packages("shinythemes")
-  library(shinythemes)
+if (!require("magrittr")) {
+  install.packages("magrittr")
+  library(magrittr)
 }
 if (!require("leaflet")) {
   install.packages("leaflet")
@@ -19,56 +19,45 @@ if (!require("leaflet.extras")) {
   install.packages("leaflet.extras")
   library(leaflet.extras)
 }
+if (!require("mapview")) {
+  install.packages("mapview")
+  library(mapview)
+}
+if (!require("leafsync")) {
+  install.packages("leafsync")
+  library(leafsync)
+}
+if (!require("tidyverse")) {
+  install.packages("tidyverse")
+  library(tidyverse)
+}
+if (!require("ggmap")) {
+  install.packages("ggmap")
+  library(ggmap)
+}
 
-# Define UI for application that draws a histogram
-shinyUI(
-    navbarPage(strong("Citi Bike Study",style="color: white;"), 
-               theme=shinytheme("cerulean"), # select your themes https://rstudio.github.io/shinythemes/
-#------------------------------- tab panel - Maps ---------------------------------
-                tabPanel("Maps",
-                         icon = icon("map-marker-alt"), #choose the icon for
-                         div(class = 'outer',
-                        # side by side plots
-                        fluidRow(
-                                splitLayout(cellWidths = c("50%", "50%"), 
-                                             leafletOutput("left_map",width="100%",height=1200),
-                                             leafletOutput("right_map",width="100%",height=1200))),
-                        #control panel on the left
-                        absolutePanel(id = "control", class = "panel panel-default", fixed = TRUE, draggable = TRUE,
-                                      top = 200, left = 50, right = "auto", bottom = "auto", width = 250, height = "auto",
-                                      tags$h4('Citi Bike Activity Comparison'), 
-                                      tags$br(),
-                                      tags$h5('Pre-covid(Left) Right(Right)'), 
-                                      prettyRadioButtons(
-                                                      inputId = "adjust_score",
-                                                      label = "Score List:", 
-                                                      choices = c("start_cnt", 
-                                                                  "end_cnt", 
-                                                                  "day_diff_absolute",
-                                                                  "day_diff_percentage"),
-                                                      inline = TRUE, 
-                                                      status = "danger",
-                                                      fill = TRUE
-                                                        ),
-                                      awesomeRadio("adjust_time", 
-                                                   label="Time",
-                                                    choices =c("Overall",
-                                                               "Weekday", 
-                                                               "Weekend"), 
-                                                    selected = "Overall",
-                                                    status = "warning"),
-                                      # selectInput('adjust_weather',
-                                      #             label = 'Adjust for Weather',
-                                      #             choices = c('Yes','No'), 
-                                      #             selected = 'Yes'
-                                      #             ),
-                                      style = "opacity: 0.80"
-                                      
-                                ), #Panel Control - Closing
-                            ) #Maps - Div closing
-                        ) #tabPanel maps closing
-   
+if (!require("shinythemes")) {
+  install.packages("shinythemes")
+  library(shinythemes)
+}
 
 
-    ) #navbarPage closing  
-) #Shiny UI closing    
+
+incident_types <- sort(unique(merged_data$incidentType))
+incident_types <- c("Total", incident_types)
+
+ui <- fluidPage(
+  titlePanel("FEMA Declarations by State"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      # Add a dropdown menu to select incident type
+      selectInput("incidentType", "Select Incident Type:",
+                  choices = incident_types),
+    ),
+    
+    mainPanel(
+      leafletOutput("map")
+    )
+  )
+)
